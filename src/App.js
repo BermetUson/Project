6a9d6a41964226d3788c223d55d40d7fd201721c
Bermet <oceaninapuddle@gmail.com>
2023-01-25 22:11:49 +0600

    page Projects created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./styles/main.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div>
       <Navbar />
 
-      <Home />
+      {/* <Home /> */}
+
+      <Projects />
 
       <Footer />
     </div>
