19a68ecb94e5b3150582b861010fc34b32de4856
Bermet <oceaninapuddle@gmail.com>
2023-01-25 22:18:06 +0600

    page Contacts created

---
+++ src/pages/Contacts.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <>
+      <main className="section">
+        <div className="container">
+          <h1 className="title-1">Contacts</h1>
+
+          <ul className="content-list">
+            <li className="content-list__item">
+              <h2 className="title-2">Location</h2>
+              <p>Bishkek, Kyrgyzstan</p>
+            </li>
+            <li className="content-list__item">
+              <h2 className="title-2">Telegram / WhatsApp</h2>
+              <p>
+                <a href="tel:+79051234567">+996 (707) 000-000</a>
+              </p>
+            </li>
+            <li className="content-list__item">
+              <h2 className="title-2">Email</h2>
+              <p>
+                <a href="mailto:usonbemet501@gmail.com">
+                  usonbemet501@gmail.com
+                </a>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Contacts;
