91715cb4bb9d9699c141762276666abae56d0581
Bermet <oceaninapuddle@gmail.com>
2023-01-26 21:28:45 +0600

    ProjectList component is created

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,44 @@
+import project01 from "./../img/projects/01.jpg";
+import project01Big from "./../img/projects/01-big.jpg";
+
+import project02 from "./../img/projects/02.jpg";
+import project02Big from "./../img/projects/02-big.jpg";
+
+import project03 from "./../img/projects/03.jpg";
+import project03Big from "./../img/projects/03-big.jpg";
+
+import project04 from "./../img/projects/04.jpg";
+import project04Big from "./../img/projects/04-big.jpg";
+
+const projects = [
+  {
+    title: "Photo collection",
+    skills: "React, JavaScript, SCSS",
+    img: project01,
+    imgBig: project01Big,
+    gitHubLink: "https://github.com/BermetUson/Photo-collection",
+  },
+  {
+    title: "List of users",
+    img: project02,
+    imgBig: project02Big,
+    skills: "React, JavaScript, SCSS",
+    gitHubLink: "https://github.com/BermetUson/List-of-users",
+  },
+  {
+    title: "Quiz",
+    img: project03,
+    imgBig: project03Big,
+    skills: "React, JavaScript, SCSS",
+    gitHubLink: "https://github.com/BermetUson/Quiz",
+  },
+  {
+    title: "Project",
+    img: project04,
+    imgBig: project04Big,
+    skills: "React, JavaScript, SCSS",
+    gitHubLink: "https://github.com/BermetUson/Project",
+  },
+];
+
+export { projects };
