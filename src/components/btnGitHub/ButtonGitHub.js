66a579db856d4a8b47776989d75fa0098fac3aee
Bermet <oceaninapuddle@gmail.com>
2023-01-26 22:56:44 +0600

    routing setup

--- src/components/btnGitHub/ButtonGitHub.js
+++ src/components/btnGitHub/ButtonGitHub.js
@@ -4,8 +4,7 @@ import gitHubBlack from "./../../img/icons/gitHub-black.svg";
 
 const ButtonGitHub = ({ link }) => {
   return (
-    // eslint-disable-next-line react/jsx-no-target-blank
-    <a href={link} target="_blank" className="btn-outline">
+    <a href={link} target="_blank" rel="noreferrer" className="btn-outline">
       <img src={gitHubBlack} alt="" />
       GitHub repo
     </a>
